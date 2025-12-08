09e0fbd69f59f19d6ed2a37e5b816e808eed878b
Bartosz Zimończyk <bartosz.zimonczyk@gmail.com>
2025-12-08 22:26:40 +0100

    bleh

--- Task8/Task8_1.py
+++ Task8/Task8_1.py
@@ -5,86 +5,60 @@ import os
 import time
 from termcolor import colored
 from collections import Counter
+import scipy
 
 with open("Task8/input_test.txt", encoding="utf-8") as file:
     data = file.read()
     junction_boxes = data.split("\n")
 
-jb_with_assignments = []
+junction_boxes_coords = []
 for k, jb in enumerate(junction_boxes):
     coords = [int(i) for i in jb.split(',')]
-    jb_with_assignments.append([coords, k, -1, -1, ''])
+    junction_boxes_coords.append(coords)
 
 def distance3d(point1: list, point2: list) -> float:
     x1, y1, z1 = point1
     x2, y2, z2 = point2
-    dist = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+    dist = np.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2))
     return float(dist)
 
-circuiut_id = 0
-for i in range(len(jb_with_assignments)):
-    first_jb, first_ass, first_dist, first_neighbor, first_id = jb_with_assignments[i]
-    distances_to_all = []
-    min_distance = -1
-    min_ass = -1
-    min_point = -1
-    for j in range(len(jb_with_assignments)):
-        if i == j:
-            continue
-        
-        second_jb, second_ass, second_dist, second_neighbor, second_id = jb_with_assignments[j]
-        dist = distance3d(first_jb, second_jb)
-        
-        if j == 0 or (i == 0 and j == 1):
-            min_distance, min_ass, min_point = dist, second_ass, j
-        else:
-            if dist < min_distance:
-                min_distance, min_ass, min_point = dist, second_ass, j
-    
-    jb_with_assignments[i][2] = min_distance
-    jb_with_assignments[i][3] = min_point
-    jb_with_assignments[i][4] = f'{min(i, min_point)}_{max(i, min_point)}'
+junction_boxes_array = np.array(junction_boxes_coords)
+jb_tree = scipy.spatial.cKDTree(junction_boxes_array)
+jb_assigned = []
+for i, coords in enumerate(junction_boxes_array):
+    neighbor = jb_tree.query(coords, k=2)
+    neighbor_dist, neighbor_id = float(neighbor[0][1]), int(neighbor[1][1])
+    print(f'For point {coords} found neighbor {neighbor_id} in distance {neighbor_dist: 10.2f}')
+    jb_assigned.append([neighbor_id, neighbor_dist, i, i, f'{min(neighbor_id, i)}-{max(neighbor_id, i)}'])
 
-jb_with_assignments_sorted = sorted(jb_with_assignments, key = lambda x: x[2])
-for jb in jb_with_assignments_sorted:
-    print(jb)
+jb_assigned_sorted = sorted(jb_assigned, key=lambda x : x[1])
 
-managed_connections = set()
-circuiut_id = 0
-for i in range(len(jb_with_assignments_sorted)):
-    jb_with_assignments_sorted = sorted(jb_with_assignments, key = lambda x: x[2])
-    first_jb, first_ass, first_dist, first_neighbor, first_id = jb_with_assignments_sorted[i]
-    if first_id in managed_connections:
-        continue
-
-    managed_connections.add(first_id)
-
-    second_jb, second_ass, second_dist, second_neighbor, second_id = jb_with_assignments[first_neighbor]
-    
-    # if first_ass == -1 and min_ass == -1:
-    #     print(f"Closest neighbor is point no. {min_point}. Both assigned to new circuit no. {circuiut_id}")
-    #     jb_with_assignments[i][1] = circuiut_id
-    #     jb_with_assignments[min_point][1] = circuiut_id
-    #     circuiut_id +=1
-    # elif min_ass == -1:
-    #     print(f"Closest neighbor is point no. {min_point}. Neighbor assigned to existing circuit no. {first_ass}")
-    #     jb_with_assignments[min_point][1] = first_ass
-    # elif first_ass == -1:
-    #     print(f"Closest neighbor is point no. {min_point}. Current box assigned to existing circuit no. {min_ass}")
-    #     jb_with_assignments[i][1] = min_ass
-    if first_ass == second_ass:
-        print(f"Both are already assigned to circuit no. {first_ass}")
-    elif first_ass != second_ass:
-        new_id = min(first_ass, second_ass)
-        print(f"Both are assigned different ciruits. Merging {first_ass} and {second_ass} to {new_id}")
-        for j in range(len(jb_with_assignments)):
-            third_jb, third_ass, third_dist, third_neighbor, third_id = jb_with_assignments[j]
-            if third_ass == second_ass:
-                jb_with_assignments[j][1] = new_id
-    
-    if len(managed_connections) == 10:
+cuircuit_id = 0
+handled_junkboxes = set()
+for i in range(len(jb_assigned_sorted)):
+    if len(handled_junkboxes) == 10:
         break
 
-data = Counter([item[1] for item in jb_with_assignments])
+    neighbor_id, neighbor_dist, current_id, current_cuircuit, current_name = jb_assigned_sorted[i]
+    n_of_n_id, _, _, neighbor_cuircuit, _ = jb_assigned[neighbor_id]
+    if current_name in handled_junkboxes:
+        continue
+    handled_junkboxes.add(current_id)
+    if n_of_n_id == current_id:
+        handled_junkboxes.add(neighbor_id)
+    if neighbor_cuircuit == current_cuircuit:
+        print(f'Current point: {current_id}. Closest neighbor: {neighbor_id}. Already in the same cuircuit: {current_cuircuit}')
+    else:
+        new_cuircuit = min(neighbor_cuircuit, current_cuircuit)
+        print(f'Current point: {current_id}. Closest neighbor: {neighbor_id}. Assigned to cuircuit: {new_cuircuit}')
+        for j in range(len(jb_assigned_sorted)):
+            this_cuircuit = jb_assigned[j][3]
+            if this_cuircuit == neighbor_cuircuit or this_cuircuit == current_cuircuit:
+                jb_assigned[j][3] = new_cuircuit          
+    jb_assigned_sorted = sorted(jb_assigned, key=lambda x : x[1])
 
+jb_assigned_sorted = sorted(jb_assigned, key=lambda x : x[1])
+for jb in jb_assigned:
+    print(jb)
+data = Counter([item[3] for item in jb_assigned])
 print(data.most_common(10))
